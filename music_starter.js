9643571de5eaec78ce5cf20a0efdc8f6019e144c
eliseallan31 <allanelis@myvuw.ac.nz>
2024-09-12 10:14:17 +1200

    day of submission
    I cleaned up my code by adding some spaces in between the code to seperate the different elements and added labels to the different parts of the code so make it easier to read.

--- music_starter.js
+++ music_starter.js
@@ -4,25 +4,34 @@ let firstRun = true
 let Discofloor = [];
 let DiscoLight = [];
 let elephant = [];
+let glitz 
 
 // vocal, drum, bass, and other are volumes ranging from 0 to 100
 function draw_one_frame(words, vocal, drum, bass, other, counter) {
   if (firstRun) {
     rectMode(CENTER);
+    //background image
     backGround = loadImage('background.png')
+   
+    //discofloor image
     Discofloor.push(loadImage('discofloor1.png')) ;
     Discofloor.push(loadImage('discofloor2.png')) ;
     Discofloor.push(loadImage('discofloor3.png')) ;
 
+    //discolight image
     DiscoLight.push(loadImage('light1.PNG'));
     DiscoLight.push(loadImage('light2.PNG'));
     DiscoLight.push(loadImage('light3.PNG'));
 
+    //elephant image
     elephant.push(loadImage('elephant1.png'));
     elephant.push(loadImage('elephant2.png'));
     elephant.push(loadImage('elephant3.png'));
     elephant.push(loadImage('elephant4.png'));
     elephant.push(loadImage('elephant5.png'));
+
+    //glitter image
+    glitz = loadImage('glitter.png')
     firstRun = false
   }
 
@@ -43,6 +52,7 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
   image(Discofloor[discoFloorFrame], 0, 0)
   pop();
 
+  //background waves 
   for (let i = 0; i < 1000; i++) {
     fill(163, 237, 107) //green
     ellipse(i * 10, bass * sin(i * 10) + 170, 5, 5)
@@ -52,9 +62,11 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
     ellipse(i * 10, drum * sin(i * 20) + 170, 7, 7)
   }
 
+  //discolight colour change
   let discoLightFrame = int(map(drum, 0, 100, 0, 2));
   image(DiscoLight[discoLightFrame], 0, 0)
 
+  //elephant movement
   push();
   let elephantFrame = int(map(vocal, 0, 100, 0, 4));
 
@@ -62,6 +74,7 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
 
   pop();
 
+  //second elephant at beat drop
   if (counter > 60 * 72) {
     elephantFrame = int(map(bass, 0, 100, 0, 4));
     push();
@@ -70,6 +83,10 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
     pop();
   }
 
+  //glitter frame 
+if(words == 'elephant') {
+  image(glitz, 0, 0)
+}
 
   
   // image(mybackgroundimage, 0, 0);
