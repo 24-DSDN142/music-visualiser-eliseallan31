9fe2282d77381a476b5c6c9d1875eed90c017dad
eliseallan31 <allanelis@myvuw.ac.nz>
2024-09-12 13:54:07 +1200

    Update music_starter.js
    I deleted the old code that I wasnt using to make the code cleaner and to remove any confusion when reading my code

--- music_starter.js
+++ music_starter.js
@@ -88,53 +88,11 @@ if(words == 'elephant') {
   image(glitz, 0, 0)
 }
 
-  
-  // image(mybackgroundimage, 0, 0);
 
 }
 
 
 
 
-// function draw_one_frame(words, vocal, drum, bass, other, counter) {
 
-
-
-
-//   // let eyeSize = map(drum,0,100,50,150)
-//   // fill(250, 65, 120)
-//   // ellipse(150,200,eyeSize,eyeSize)
-//   // ellipse(350,200,eyeSize,eyeSize)
-// }
-
-
-
-  //  // vocal bar is red
-  //  fill(200, 0, 0);
-  //  rect(bar_pos_x, height / 2 + 1 * bar_spacing, 4 * vocal, bar_height);
-  //  fill(0);
-  //  text("vocals", bar_pos_x, height / 2 + 1 * bar_spacing + 8);
-
-  //  // drum bar is green
-  //  fill(0, 200, 0);
-  //  rect(bar_pos_x, height / 2 + 2 * bar_spacing, 4 * drum, bar_height);
-  //  fill(0);
-  //  text("drums", bar_pos_x, height / 2 + 2 * bar_spacing + 8);
-
-  //  // bass bar is blue
-  //  fill(50, 50, 240);
-  //  rect(bar_pos_x, height / 2 + 3 * bar_spacing, 4 * bass, bar_height);
-  //  fill(0);
-  //  text("bass", bar_pos_x, height / 2 + 3 * bar_spacing + 8);
-
-  //  // other bar is white
-  //  fill(200, 200, 200);
-  //  rect(bar_pos_x, height / 2 + 4 * bar_spacing, 4 * other, bar_height);
-  //  fill(0);
-  //  text("other", bar_pos_x, height / 2 + 4 * bar_spacing + 8);
-  //  fill(255, 255, 0);
-
-  //  // display "words"
-  //  textAlign(CENTER);
-  //  textSize(vocal);
-  //  text(words, width/2, height/3);
+  
